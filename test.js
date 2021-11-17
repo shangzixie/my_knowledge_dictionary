675c81ccaa7a858284640d9c772f6034bc5ba77d
shangzixie <shangzi.x@columbia.edu>
2021-11-17 10:47:26 +0800

    python list implement

--- test.js
+++ test.js
@@ -1,62 +1,39 @@
-// n m, array.length = n;
-// arr is integer
-// sum( arr[i] + ... + arr[j]) <= m
-// return [i, j]
 
-// [1, 1, 1] m = 2
-// [1, 1, 1] m = 3;
+// event dp[x] = dp[x- 2] + 2
+/*
+dp[m] = min(dp[q] + q, d[w] + w)
+i: a, b, c
 
-// [10, 1]  m = 5;
-// [1 ,1] m = 0;
-// [0],  m = 0
+m: i + a = m , dp[m] = min(dp[i] + 1 , ... )
+  dp[m + a] = min(dp[m] + 1, dp[m+a])
 
-var solution = function(arr, m, n) {
-  if (arr.length === 0) return null;
-  if (arr.length === 1) return arr[0] <= m ? [0, 0] : null;
+m - i = set(a)
 
-  let left = 0;
-  let right = 0;
-  let sum = arr[0];
-  let result = [left, right];
-  let maxSum = 0;
+m set= {a, b, c} -> m + a,  m + c, m + c
+map = {m+a: m}
 
-  // get max sum, sum <= m
-  while (right < n && sum <= m) {
-    if (right != left) sum += arr[right];
-    right += 1;
+m + a, min(dp[v1]+ 1, +dp[v2] + 1)
+
+m, set(a, b, c),       m + a | m+b | m+c
+map { m + a: m, m + b: m...}
+
+dp[m+a] = min(dp[m] + 1, ...) O(n)
+4 3
+
+*/
+
+var solution = function(i, j) {
+  if (i == j) return 0;
+  let dp = new Array(i * 2 + 1);
+  dp[i] = 0;
+
+  if (i >= j) {
+    for (let )
   }
-
-  maxSum = right === n ? sum : sum - arr[right - 1];
-  result = [left, right - 1];
-
-  // [10 , 1, 1] m = 2
-  //      ||
-  // init: sum > m
-  while (left < arr.length && right < arr.length) {
-    // move left
-    while (left < right && sum > m) {
-      sum -= arr[left];
-      left += 1;
-      // maintain maxSum, update result
-      if (sum > maxSum && sum <= m) {
-        maxSum = sum;
-        result = [left, right];
-        if (sum === m) return result;
-      }
-    }
-
-    if (left === right) sum = arr[left];
-    // sum <= m
-    while (right < arr.length && sum <= m) {
-      right += 1;
-      sum += arr[right];
-      if (sum > maxSum && sum <= m) {
-        maxSum = sum;
-        result = [left, right];
-      }
-    }
-  }
-
-  if (maxSum === 0) return null;
-  return result;
+  for (int m)
 }
+
+
+var get
+
+// x
