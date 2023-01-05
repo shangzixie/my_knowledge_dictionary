98435d99bad046c12d4a7426687ac90748d325f8
xshangzi <xshangzi@vmware.com>
2023-01-05 15:57:47 +0800

    test

--- test.js
+++ test.js
@@ -11,3 +11,7 @@ testDebounce = () => {
 document.onmousemove = () => {
   debounce(testDebounce, 1000);
 }
+/*
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4
+*/
--- test.sql
+++ test.sql
@@ -1,68 +1,17 @@
-Expected
-        <string>: COPY (
-        SELECT
-        	row_number() over(ORDER BY index_bloat_space DESC, index_bloat_rate DESC, schema, relation_name, index_name) AS Rank,
-        	database,
-        	schema,
-        	relation_name,
-        	index_name,
-        	real_size,
-        	index_bloat_space,
-        	index_bloat_rate,
-        	last_reindexed,
-        	last_scan_ts
-        FROM (
-        	SELECT
-        		database,
-        		schema,
-        		table_oid,
-        		relation_name,
-        		index_oid,
-        		index_name,
-        		real_size,
-        		index_bloat_space,
-        		index_bloat_rate,
-        		last_reindexed,
-        		last_scan_ts,
-        		is_na
-        	FROM gpmetrics.gpcc_index_scan_info AS t
-        	WHERE index_bloat_rate >= 50.000000 AND is_na is FALSE
-        		AND t.dbid = 123
-        		AND t.schema = 'test'
-        ) AS dt
-        ORDER BY 1
-        LIMIT 1000) TO 'gpmetrics/report-for-index-bloat.csv' WITH CSV DELIMITER E',' HEADER
-    to contain substring
-        <string>: COPY (SELECT
-        							row_number() over(ORDER BY index_bloat_space DESC, index_bloat_rate DESC, schema, relation_name, index_name) AS Rank,
-        							database,
-        							schema,
-        							relation_name,
-        							index_name,
-        							real_size,
-        							index_bloat_space,
-        							index_bloat_rate,
-        							last_reindexed,
-        							last_scan_ts
-        						FROM (
-        							SELECT
-        								database,
-        								schema,
-        								table_oid,
-        								relation_name,
-        								index_oid,
-        								index_name,
-        								real_size,
-        								index_bloat_space,
-        								index_bloat_rate,
-        								last_reindexed,
-        								last_scan_ts,
-        								is_na
-        							FROM gpmetrics.gpcc_index_scan_info AS t
-        							WHERE index_bloat_rate >= 0.5 AND is_na is FALSE
-        							AND t.dbid = 12
-        							AND t.schema = 'testschema'
-        						) AS dt
-        						ORDER BY 1
-        						LIMIT 100
-        ) TO 'gpmetrics/report-for-index-bloat.csv' WITH CSV DELIMITER E',' HEADER
+CREATE TABLE gpmetrics.gpcc_index_info (
+	  dbid 						OID
+	, database 					VARCHAR(64)
+	, schema 					VARCHAR(64)
+	, table_oid 				OID
+	, relation_name				VARCHAR(64)
+	, index_oid 				OID
+	, index_name				VARCHAR(64)
+	, real_size					BIGINT
+	, extra_size				BIGINT
+	, fillfactor				INT
+	, index_bloat_space			BIGINT
+	, index_bloat_rate			DOUBLE PRECISION
+	, is_na						BOOLEAN
+	, last_scan_ts				TIMESTAMP WITH TIME ZONE
+	, last_reindexed			TIMESTAMP WITH TIME ZONE
+) DISTRIBUTED BY (dbid, table_oid, index_oid);
