523c44aad6e2d34bd86edffbcf59c408978630d1
xshangzi <xshangzi@vmware.com>
2023-02-16 16:02:04 +0800

    work

--- test.sql
+++ test.sql
@@ -5,10 +5,19 @@ $$ LANGUAGE SQL;
 CREATE FUNCTION sum_n_product_with_tab (x int, OUT sum int, OUT product int)
 RETURNS SETOF record
 AS $$
-    SELECT $1 + tab.y, $1 * tab.y FROM tab;
+    SELECT $1 + test_new_query.y, $1 * test_new_query.y FROM test_new_query;
 $$ LANGUAGE SQL;
 
 CREATE TABLE foo (fooid int, foosubid int, fooname text);
 INSERT INTO foo VALUES (1, 1, 'Joe');
 INSERT INTO foo VALUES (1, 2, 'Ed');
 INSERT INTO foo VALUES (2, 1, 'Mary');
+
+
+UPDATE test_new_query
+SET y = 20
+WHERE y = 1;
+
+UPDATE test_new_query
+SET (y, z) = (20, 10)
+WHERE y = 20;
